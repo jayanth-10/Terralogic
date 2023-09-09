79bea61e29828e3706fe9919f50bdf694dbbb45a
Thota Jayanth <105880711+jayanth-10@users.noreply.github.com>
2023-09-09 17:54:43 +0530

    Add files via upload

---
+++ Terralogic/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".multiple-text",{
+  strings: ["Cyber Security Engineer","Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 100,
+  loop: true
+})
+
+
+
+
+
+  
